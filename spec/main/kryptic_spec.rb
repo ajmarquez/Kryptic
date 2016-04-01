61a8b97bef65089da257b782325071901d27bcea
Abelardo <ajmarquezg@gmail.com>
2016-04-01 18:16:02 -0430

    Creates Kryptic_spec.rb

--- spec/main/kryptic_spec.rb
+++ spec/main/kryptic_spec.rb
@@ -1,18 +1,65 @@
 require 'spec_helper'
 
 
-describe 'The HelloWorld App' do
+describe 'Kryptic App' do
   include Rack::Test::Methods
 
   def app
-    Sinatra::Application
+    KrypticApp
   end
 
-  it "says hello" do
-    get '/'
-    expect(last_response).to be_ok
-    expect(last_response.body).to eq('Hello World')
+ ## SEND DOMAIN
+  describe "#availability" do
 
+    context "when domain is found" do
+      it "returns TRUE"
+    end
+
+    context "when domain is NOT found" do
+      it "answers FALSE" 
+    end
+  end
+
+
+
+ ## POST SAVE
+  describe "#save_content" do
+
+    context "when right hash is received" do
+      it "returns confirmation"
+    end
+
+    context "when WRONG hash is received" do
+      it "returns NO GO"
+    end
+  end
+
+
+
+  ## POST Delete_site
+  describe "#delete_site" do
+
+    context "when right hash is received" do
+      it "returns confirmation"
+    end
+
+    context "when WRONG hash is recieved" do
+      it "returns NO GO"
+    end
+  end
+
+
+
+  ## GET reload
+  describe "#reload" do
+
+    context "when right hash is received" do
+      it "returns confirmation"
+    end
+
+    context "when WRONG hash is recieved" do
+      it "returns NO GO"
+    end
   end
 
 end
